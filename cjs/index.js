afa8ee4684bc47758caa5efdd4130cfa02bf4c67
Andrés Dasvid <andresdavi.dev.1405@gmail.com>
2026-01-17 14:57:22 -0500

    org

--- cjs/index.js
+++
@@ -1,8 +0,0 @@
-// CommonJS require module
-
-// .js --> or defecto utiliza CommonJs
-// .mja --> para utiliza EcmaScript Modules
-// .cja --> pra utilizar CommonJS
-const {sum} = require('./sum.mja')
-
-console.log(sum(1, 2))
--- mjs/index.mjs
+++
@@ -1,5 +0,0 @@
-import {sum, sub, mult} from './sum.mjs'
-
-console.log(sum(1,2))
-console.log(sub(1,2))
-console.log(mult(1,2))
